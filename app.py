de905cb34dea34c45b2c9b955b116707fdb5d255
Michael Warmbier <MichaelCWarmbier@gmail.com>
2022-08-11 21:35:25 +0000

    added remaining functions

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import re
 with open('Collins Scrabble Words (2019).txt') as file:
     wordList = file.read().splitlines()
 
-# Constant letter point values
-class letters:
+## Constant letter point values
+class letterValue:
   values = [[' '], 
             ['A','E','I','L','N','O','R','S','T','U'],
             ['D','G'],
@@ -30,11 +30,11 @@ class tempMemory:
 
 ##### Methods #####
 
-# Returns point value of a specific letter
+## Returns point value of a specific letter
 def getLetterValue(letter):
-  for value in letters.values:
-    if letter.upper() in letters.values[letters.values.index(value)]:
-      return letters.values.index(value);
+  for value in letterValue.values:
+    if letter.upper() in letterValue.values[letterValue.values.index(value)]:
+      return letterValue.values.index(value);
   raise Exception('Argument is NOT a valid Scrabble letter.');
 
 def getWordValue(word):
@@ -42,24 +42,59 @@ def getWordValue(word):
   for char in word:
     finalValue += getLetterValue(char);
   return finalValue;
-    
 
-# Gets all possible words given provided letters
+## Create table of letters and appearances
+def createLetterTable(string):
+  letterTable = [];
+  index = 0;
+  for letter in string: 
+    letterTable.append([])
+    letterTable[index].append(letter);
+    letterTable[index].append(1)
+    for entry in letterTable:
+      if letterTable[index][0] == entry[0] and index != letterTable.index(entry):
+        letterTable.remove(letterTable[index]);
+        letterTable[letterTable.index(entry)][1] += 1;
+        index -= 1;
+    index += 1;
+  return letterTable;
+
+## Filters letters based on total
+def filterRepeats(wordList, letters):
+  table = createLetterTable(letters);
+  for word in wordList[:]:
+    validWord = True;
+    wordTable = createLetterTable(word);
+    
+    for wordEntry in wordTable:
+      for entry in table:
+        if wordEntry[0] == entry[0] and wordEntry[1] != entry[1]:
+          validWord = False;
+    if not validWord: 
+      wordList.remove(word);
+  return wordList;
+
+## Gets all possible words given provided letters
 def getWords(letters):
+  if (len(letters)< 7 or len(letters) > 15):
+    raise Exception ('Letter count must be between seven and fifteen.')
+    
   regex = re.compile('^([' + letters.upper() + '])*$');
   validWords = [];
   for word in wordList:
     if regex.match(word):
       validWords.append(word);
   for word in validWords:
-    tLetters = letters
+    tLetters = letters;
     for char in word:
-      tLetters.replace(char, '')
-      
+      tLetters.replace(char, '');
+  validWords = filterRepeats(validWords, letters);
+  for word in validWords[:]:
+    validWords[validWords.index(word)] = [word, getWordValue(word)];
   return validWords;
     
 ##### Test Code #####
 
-storedWords = getWords('juhsine');
+storedWords = getWords('JUHSINE');
 for word in storedWords:
-  print(word + ' ' + str(getWordValue(word)))
+  print(str(word) + '\n')
